6c3ed7243d1a45c7194055430439da74678be926
AliDeWeb <alimoradi0business@gmail.com>
2024-08-07 07:55:20 -0700

    container configs set

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: { center: true, padding: "1rem" },
     extend: {
       fontFamily: {
         poppins: ["var(--font-poppins)"],
