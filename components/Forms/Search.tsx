251bd99625a4c6d1692f2638ba31785836045ae7
AliDeWeb <alimoradi0business@gmail.com>
2024-08-08 00:04:10 -0700

    search input logic changed

--- components/Forms/Search.tsx
+++ components/Forms/Search.tsx
@@ -1,34 +1,26 @@
 "use client";
 
-import React, { ChangeEvent } from "react";
+import React from "react";
 
 // Icons
 import { IoSearchOutline } from "react-icons/io5";
 
 const Search = () => {
-  // States
-  const [searchValue, setSearchValue] = React.useState<string>("");
+  // Refs
+  const searchInput = React.useRef<null | HTMLInputElement>(null);
 
   // Callbacks
   const searchHandler = React.useCallback(() => {
-    if (!searchValue.trim().length) return;
+    if (!searchInput || !searchInput.current?.value.trim()) return;
 
     //   TODO: the value of `searchValue` should be trim before any action
-    console.log(searchValue);
-  }, [searchValue]);
-  const searchValueChangeHandler = React.useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      setSearchValue((e.target as HTMLInputElement).value || "");
-    },
-    []
-  );
+    console.log(searchInput.current?.value);
+  }, []);
 
   return (
     <div className="flex items-center justify-between w-[500px] px-4 py-2.5 border border-solid border-[#3474d4] text-[#555555]">
       <input
-        defaultValue={searchValue}
-        value={searchValue}
-        onChange={searchValueChangeHandler}
+        ref={searchInput}
         type="text"
         placeholder="search products..."
         className={`font-poppins font-normal text-sm outline-none border-none w-[calc(100%-30px)]`}
