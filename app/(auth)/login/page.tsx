93286a9ed6f5838955c55bc7241ac2f10178b7d2
AliDeWeb <alimoradi0business@gmail.com>
2024-08-09 00:28:09 -0700

    metadata added to auth pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+import type { Metadata } from 'next';
+
+// Metadata
+export const metadata: Metadata = {
+  title: 'Ekka - Login',
+};
 
 const Login = () => {
   return (
